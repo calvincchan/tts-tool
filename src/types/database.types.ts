268f6b9d95f41059fd28efe88ec49f33f14af9bf
Calvin C Chan <hello@calvin-c.com>
2025-05-28 13:57:32 -0700

    feat: add type definitions for SpeechScript and CustomTheme

---
+++ src/types/database.types.ts
@@ -0,0 +1,9 @@
+[?25l
+    Select a project:                                                                                                  
+                                                                                                                       
+  >  1. adodrwirdngxnjiwcmyl [name: familogue-app, org: medhalsupyhhprthuolg, region: us-east-1]                       
+    2. uupivloqxgsuxfcnnoea [name: howardlau.work@gmail.com's Project, org: hljrljrevyjctlevkzjb, region: ca-central-1]
+                                                                                                                       
+                                                                                                                       
+    ↑/k up • ↓/j down • / filter • q quit • ? more                                                                     
+                                                                                                                       [0D
